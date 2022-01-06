e1643f9edfecd01dc90110e31a1ff24a55baaaab
Liam Steiner <liams@moveo.co.il>
2022-01-06 22:16:45 +0200

    Changed port to 8081 to match nginx 1.4.7

--- dist/configFile.js
+++ dist/configFile.js
@@ -2,7 +2,7 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.config = void 0;
 exports.config = {
-    PORT: 5000,
+    PORT: 8081,
     MONGO_URI: "mongodb+srv://sliamh11:liamstein1234@liams-cluster.k2siq.mongodb.net/epicure",
     JWT_KEY: "shhhh"
 };
